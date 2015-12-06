4dfb40bff28ecc8be5f36b4ce5298bfd77a132e1
fafrd <kbradley@ucmerced.edu>
2015-12-05 18:07:03 -0800

    colors

---
+++ map.js
@@ -0,0 +1,114 @@
+// STYLES
+
+var roadkillStyles = {
+	'Amphibian':
+	[new ol.style.Style({
+		image: new ol.style.Circle({
+			radius: 5,
+			fill: new ol.style.Fill({
+			color: 'violet'
+		}),
+			stroke: new ol.style.Stroke({
+			color: 'violet',
+			width: 1
+			})
+		})
+	})],
+	'Bird':
+	[new ol.style.Style({
+		image: new ol.style.Circle({
+			radius: 5,
+			fill: new ol.style.Fill({
+			color: 'yellow'
+		}),
+			stroke: new ol.style.Stroke({
+			color: 'yellow',
+			width: 1
+			})
+		})
+	})],
+	'Mammal (Large)': [new ol.style.Style({
+		image: new ol.style.Circle({
+			radius: 5,
+			fill: new ol.style.Fill({
+			color: 'orange'
+		}),
+			stroke: new ol.style.Stroke({
+			color: 'orange',
+			width: 1
+			})
+		})
+	})],
+	'Mammal (Medium)': [new ol.style.Style({
+		image: new ol.style.Circle({
+			radius: 5,
+			fill: new ol.style.Fill({
+			color: 'darkblue'
+		}),
+			stroke: new ol.style.Stroke({
+			color: 'blue',
+			width: 1
+			})
+		})
+	})],
+	'Mammal (Small)': [new ol.style.Style({
+		image: new ol.style.Circle({
+			radius: 5,
+			fill: new ol.style.Fill({
+			color: 'blue'
+		}),
+			stroke: new ol.style.Stroke({
+			color: 'blue',
+			width: 1
+			})
+		})
+	})],
+	'Reptile': [new ol.style.Style({
+		image: new ol.style.Circle({
+			radius: 5,
+			fill: new ol.style.Fill({
+			color: 'green'
+		}),
+			stroke: new ol.style.Stroke({
+			color: 'green',
+			width: 1
+			})
+		})
+	})]
+}
+
+var styleFunction = function(feature, resolution) {
+	return roadkillStyles[feature.getProperties().Species_Ca]
+}
+
+// LAYERS
+var layer_basemap = new ol.layer.Tile({
+	source: new ol.source.OSM()
+})
+
+var layer_CROS = new ol.layer.Vector({
+	source: new ol.source.Vector({
+		url: 'data/cros.geojson', 
+		format: new ol.format.GeoJSON({
+			defaultDataProjection: 'EPSG:4326', 
+			projection: 'EPSG:3857'
+		})
+	}),
+	name: 'CROS',
+	style: styleFunction
+	//visible: false
+})
+
+// MAP
+
+var map = new ol.Map({
+	target: 'map',
+	layers: [
+		layer_basemap, 
+		layer_CROS 
+	],
+	view: new ol.View({
+		center: ol.proj.fromLonLat([-120, 37.3]),
+		zoom: 5.5
+	})
+});
