a746e367182dc0791057f8d12b447bc97a6df44a
fafrd <kbradley@ucmerced.edu>
2015-12-06 16:17:43 -0800

    added data

--- map.js
+++ map.js
@@ -81,23 +81,104 @@ var styleFunction = function(feature, resolution) {
 	return roadkillStyles[feature.getProperties().Species_Ca]
 }
 
+var styleFunction_birds = function(feature, resolution) {
+	return [new ol.style.Style({
+		image: new ol.style.Circle({
+			radius: (feature.getProperties().SENSITIVE_)/1.5,
+			fill: new ol.style.Fill({
+                color: 'rgba(63, 127, 191, 0.54)'
+            })
+		})
+	})]
+}
+
 // LAYERS
 var layer_basemap = new ol.layer.Tile({
 	source: new ol.source.OSM()
 })
 
-var layer_CROS = new ol.layer.Vector({
+var layer_CROS_amphibian = new ol.layer.Vector({
 	source: new ol.source.Vector({
-		url: 'data/cros.geojson', 
+		url: 'data/cros/cros_amphibian.geojson', 
 		format: new ol.format.GeoJSON({
 			defaultDataProjection: 'EPSG:4326', 
 			projection: 'EPSG:3857'
 		})
 	}),
 	eventListeners: layerListeners,
-	name: 'CROS',
+	name: 'California Roadkill Observation System - Amphibians',
 	style: styleFunction
-	//visible: false
+	//,visible: false
+})
+
+var layer_CROS_bird = new ol.layer.Vector({
+	source: new ol.source.Vector({
+		url: 'data/cros/cros_bird.geojson', 
+		format: new ol.format.GeoJSON({
+			defaultDataProjection: 'EPSG:4326', 
+			projection: 'EPSG:3857'
+		})
+	}),
+	eventListeners: layerListeners,
+	name: 'California Roadkill Observation System - Birds',
+	style: styleFunction
+	//,visible: false
+})
+
+var layer_CROS_mammal_lg = new ol.layer.Vector({
+	source: new ol.source.Vector({
+		url: 'data/cros/cros_mammal_lg.geojson', 
+		format: new ol.format.GeoJSON({
+			defaultDataProjection: 'EPSG:4326', 
+			projection: 'EPSG:3857'
+		})
+	}),
+	eventListeners: layerListeners,
+	name: 'California Roadkill Observation System - Mammals (Large)',
+	style: styleFunction
+	//,visible: false
+})
+
+var layer_CROS_mammal_med = new ol.layer.Vector({
+	source: new ol.source.Vector({
+		url: 'data/cros/cros_mammal_med.geojson', 
+		format: new ol.format.GeoJSON({
+			defaultDataProjection: 'EPSG:4326', 
+			projection: 'EPSG:3857'
+		})
+	}),
+	eventListeners: layerListeners,
+	name: 'California Roadkill Observation System - Mammals (Medium)',
+	style: styleFunction
+	//,visible: false
+})
+
+var layer_CROS_mammal_sm = new ol.layer.Vector({
+	source: new ol.source.Vector({
+		url: 'data/cros/cros_mammal_sm.geojson', 
+		format: new ol.format.GeoJSON({
+			defaultDataProjection: 'EPSG:4326', 
+			projection: 'EPSG:3857'
+		})
+	}),
+	eventListeners: layerListeners,
+	name: 'California Roadkill Observation System - Mammals (Small)',
+	style: styleFunction
+	//,visible: false
+})
+
+var layer_CROS_reptile = new ol.layer.Vector({
+	source: new ol.source.Vector({
+		url: 'data/cros/cros_reptile.geojson', 
+		format: new ol.format.GeoJSON({
+			defaultDataProjection: 'EPSG:4326', 
+			projection: 'EPSG:3857'
+		})
+	}),
+	eventListeners: layerListeners,
+	name: 'California Roadkill Observation System - Reptiles',
+	style: styleFunction
+	//,visible: false
 })
 
 var layer_cdfw = new ol.layer.Vector({
@@ -108,10 +189,37 @@ var layer_cdfw = new ol.layer.Vector({
 			projection: 'EPSG:3857'
 		})
 	}),
-	name: 'CROS'
+	name: 'California Department of Fish and Wildlife Regions'
 	//style: styleFunction
-	//visible: false
+	,visible: false
 })
+
+var layer_mammaltracks = new ol.layer.Vector({
+	source: new ol.source.Vector({
+		url: 'data/ds442.geojson', 
+		format: new ol.format.GeoJSON({
+			defaultDataProjection: 'EPSG:4326', 
+			projection: 'EPSG:3857'
+		})
+	}),
+	name: 'Mammal tracks counts in San Diego and LA Areas'
+	//style: styleFunction
+	,visible: false
+})
+
+var layer_birdareas = new ol.layer.Vector({
+	source: new ol.source.Vector({
+		url: 'data/ds078.geojson', 
+		format: new ol.format.GeoJSON({
+			defaultDataProjection: 'EPSG:4326', 
+			projection: 'EPSG:3857'
+		})
+	}),
+	name: 'Important bird areas',
+	style: styleFunction_birds
+	//,visible: false
+})
+
 //Layer Listeners
 var layerListeners = {
     featureclick: function(e) {
@@ -129,8 +237,15 @@ var map = new ol.Map({
 	target: 'map',
 	layers: [
 		layer_basemap, 
+		layer_CROS_amphibian, 
+		layer_CROS_bird, 
+		layer_CROS_mammal_lg, 
+		layer_CROS_mammal_med, 
+		layer_CROS_mammal_sm, 
+		layer_CROS_reptile, 
 		layer_cdfw,
-		layer_CROS 
+        layer_mammaltracks,
+        layer_birdareas
 	],
 	view: new ol.View({
 		center: ol.proj.fromLonLat([-120, 37.3]),
